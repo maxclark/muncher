8b6ba3879173e91201ff5fca69afef1b1a5e7e2c
Max Clark <max@clarksys.com>
2012-08-29 15:51:28 -0700

    clean up of some old, ugly code.

--- muncher.pl
+++ muncher.pl
@@ -1,10 +1,34 @@
 #!/usr/bin/perl -w
 
-my $logfile = "found_emails";
+# muncher.pl: search for email addresses in files
+#
+#    Copyright (C) 2012 Max Clark and Creative Thought, Inc.
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
+# Perl Modules
+# ------------
 package Email::Find::Loose;
 use base qw(Email::Find);
 use Email::Valid::Loose;
+
+my $VERSION = "0.1";
+
+# Configuration
+# -------------
+my $outfile = "/tmp/found_emails.txt";
 
 # should return regex, which Email::Find will use in finding
 # strings which are "thought to be" email addresses
@@ -20,16 +44,22 @@ sub do_validate {
         return Email::Valid::Loose->address($addr);
 }
 
-open (LOG, ">>$logfile") || die "cannot appent: $!";
+open (OUT, ">>$outfile") || die "cannot append: $!";
 
 # Simply print out all the addresses found leaving the text undisturbed.
 my $finder = Email::Find::Loose->new(sub {
         my($email, $orig_email) = @_;
-        print LOG $email->format."\n";
+        print OUT $email->format."\n";
 });
 
-while (<>) {
+my $file = shift @ARGV;
+
+open (IN, "<$file") || die "$!";
+
+while (<IN>) {
         $finder->find(\$_);
 }
 
-close (LOG) || die "$!";
+close (IN) || die "$!";
+
+close (OUT) || die "$!";
---
+++ unique.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+
+# unique.pl: find and display unique lines
+#
+#    Copyright (C) 2012 Max Clark and Creative Thought, Inc.
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+use locale;
+
+%seen = ();
+
+foreach (<>) {
+
+	# Change all letters to lowercase
+	# --
+	s/([^\W0-9_])/\l$1/g;
+
+	# Parse for unique names
+	# --
+	print $_ unless $seen{$_}++;
+
+}
